938920a53f03de5deba5392540ed4d057690540d
Kenzou <dangchithanh780@gmail.com>
2024-01-02 00:50:10 +0700

    Create Entity Employee

---
+++ main/java/dev/kenzou/ems/entity/Employee.java
@@ -0,0 +1,25 @@
+package dev.kenzou.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email_id")
+    private String emailId;
+}
